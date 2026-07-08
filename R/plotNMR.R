728add85a9343fe7a65adccd6d4541862cb0f0b2
Gomes Da Graca <ggomesda@ic.ac.uk>
2026-07-08 15:43:03 +0100

    ppm scale can be added as a numeric vector

--- R/plotNMR.R
+++ R/plotNMR.R
@@ -12,9 +12,11 @@
 #' prior to plotting.
 #' @param f1 Chemical shift of the leftmost ppm value to plot.
 #' @param f2 Chemical shift of the rightmost ppm value to plot. 
-#' @param ppm Logical value. If \code{TRUE} the ppm values will be  
-#' as the plot x scale (default option). The variable indexes will used if
-#' set to \code{FALSE}.
+#' @param ppm If set to \code{TRUE} (default option) the ppm values will be  
+#' added to the plot x scale. The variable indexes will used if
+#' set to \code{FALSE}. The chemical shift values can also be added as a
+#' numeric vector of the same size as the number of rows or columns of
+#' \code{X}.
 #' @param title Title of the plot (optional).
 #'
 #' @return A stacked plot of the full spectra or spectra region.
@@ -23,23 +25,34 @@
 #' plotNMR(urine)
 #' 
 #' @export
-plotNMR<-function(X, f1 = max(X[,1]), f2 = min(X[,1]), ppm = TRUE, title = ""){
+plotNMR <- function(X, f1=max(X[,1]), f2=min(X[,1]), ppm=TRUE, title=""){
 	 #if the matrix is not in the format [variables,samples] it will be transposed
     if(dim(X)[1] < dim(X)[2]){
         X<-t(X)
     }
-    lim <-which(X[,1]>f2 & X[,1]<f1)
-    l2 <-lim[1]
-    l1 <-lim[length(lim)]
-    matplot(X[l1:l2,1], X[l1:l2,2:dim(X)[2]], type = "l", 
-            xlim=rev(range(X[l1:l2,1])),
+	if(length(ppm) > 1){
+		ppm <- as.numeric(ppm)
+        lim <- which(ppm > f2 & ppm < f1)
+        l2 <-lim[1]
+        l1 <-lim[length(lim)]
+        matplot(ppm[l1:l2], X[l1:l2,], type = "l", 
+            xlim=rev(range(ppm[l1:l2])),
             xlab="chemical shift (ppm)", 
             ylab="intensity (a.u.)", main=title, lty=1)
-    if(!ppm){
-        xscale <- lim
-        matplot(xscale, X[l1:l2,2:dim(X)[2]], type="l",
-            xlab="variable index", 
-            ylab="intensity (a.u.)", 
-            main=title, lty=1)
-  }
+	} else {
+        lim <- which(X[,1]>f2 & X[,1]<f1)
+        l2 <- lim[1]
+        l1 <- lim[length(lim)]
+        matplot(X[l1:l2,1], X[l1:l2,2:dim(X)[2]], type = "l", 
+                xlim=rev(range(X[l1:l2,1])),
+                xlab="chemical shift (ppm)", 
+                ylab="intensity (a.u.)", main=title, lty=1)
+        if(isFALSE(ppm)){
+            xscale <- lim
+            matplot(xscale, X[l1:l2,2:dim(X)[2]], type="l",
+                xlab="variable index", 
+                ylab="intensity (a.u.)", 
+                main=title, lty=1)
+        }
+	}
 }
